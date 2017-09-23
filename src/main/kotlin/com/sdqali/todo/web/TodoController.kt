2ed8f4fec665f53592d19f11f651100e4b20cc2b
Sadique Ali <sadiqalikm@gmail.com>
2017-09-22 23:09:39 -0700

    Implement list and clear end points.

--- src/main/kotlin/com/sdqali/todo/web/TodoController.kt
+++ src/main/kotlin/com/sdqali/todo/web/TodoController.kt
@@ -1,12 +1,21 @@
 package com.sdqali.todo.web
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class TodoController {
     @GetMapping("/")
-    fun list(): String {
+    fun list(): List<String> {
+        return emptyList()
+    }
+
+    @PostMapping("/")
+    fun create(@RequestBody input: Map<String, String>): Map<String, String> {
+        return input
+    }
+
+    @DeleteMapping("/")
+    fun clear(): String {
         return ""
     }
 }
--- src/test/kotlin/com/sdqali/todo/web/TodoControllerTest.kt
+++ src/test/kotlin/com/sdqali/todo/web/TodoControllerTest.kt
@@ -1,15 +1,18 @@
 package com.sdqali.todo.web
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -17,6 +20,9 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 class TodoControllerTest {
     @Autowired
     private lateinit var wac: WebApplicationContext
+
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
 
     private lateinit var mvc: MockMvc
 
@@ -30,5 +36,33 @@ class TodoControllerTest {
     fun canListItems() {
         mvc.perform(get("/"))
             .andExpect(status().isOk)
+    }
+
+    @Test
+    fun acceptsAnItemAndReturnsIt() {
+        mvc.perform(post("/")
+            .content(objectMapper.writeValueAsBytes(mapOf("title" to "do something")))
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk)
+    }
+
+    @Test
+    fun canClearItems() {
+        mvc.perform(delete("/")
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk)
+    }
+
+    @Test
+    fun verifyListIsCleared() {
+        mvc.perform(delete("/")
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk)
+
+        mvc.perform(get("/")
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk)
+            .andExpect(content().bytes(objectMapper.writeValueAsBytes(emptyArray<String>())))
+
     }
 }
